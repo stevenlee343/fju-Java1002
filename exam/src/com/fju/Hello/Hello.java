0c97c3e0de9e033b5bfe454d9de855a7c95dcefd
steven <stxx0304@gmail.com>
2018-10-02 10:57:01 +0800

    fju java exam2

--- exam/src/com/fju/Hello/Hello.java
+++ exam/src/com/fju/Hello/Hello.java
@@ -4,6 +4,18 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		int age, height, weight;
+		age = 20;
+		height = 175;
+		weight = 65;
+		float num = 10.2f;
+		String name = "Steven";
+		
+		System.out.print("I am " + age + " years old, ");
+		System.out.print(height + "cm tall ");
+		System.out.println("and " + weight + " kg");
+		System.out.println(num);
+		System.out.println(name);
 
 	}
 
